dd80494edbb6463b4584cc61ff472d756e48fbe7
ResoDevansh <sachandevansh2000@gmail.com>
2023-03-21 16:31:28 +0530

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -31,6 +31,23 @@ const Login = () => {
         </Nav>
         <JoinNow>Join now</JoinNow>
         <SignIn>Sign in</SignIn>
+        <Content>
+          <Heading>
+            Welcome to your <br />
+            professional community
+          </Heading>
+          <Form>
+            <label htmlFor="email">Email or phone</label>
+            <input id="email" type="email"></input>
+            <br></br>
+            <label htmlFor="password">Password</label>
+            <input id="password" type="password"></input>
+            <ForgotPassword>Forgot password?</ForgotPassword>
+            <SignInButton>Sign in</SignInButton>
+            <Line><hr/>or<hr/></Line>
+            <Google><img src="/images/google.svg"></img>Sign in with Google</Google>
+          </Form>
+        </Content>
       </Container>
     </>
   );
@@ -108,7 +125,7 @@ const Nav = styled.div`
 `;
 const Bar = styled.div`
   height: 5vh;
-  border: .001em solid #d9d9d9;
+  border: 0.001em solid #d9d9d9;
   position: absolute;
   left: 71.5vw;
   top: 2.5vh;
@@ -118,25 +135,110 @@ const JoinNow = styled.button`
   position: absolute;
   left: 72vw;
   top: 2vh;
-  padding: .8em 1.5em;
+  padding: 0.8em 1.5em;
   border-radius: 2em;
   background-color: transparent;
-   font-weight: 500;
-   opacity: .9;
-   &:hover{
+  font-weight: 500;
+  opacity: 0.9;
+  &:hover {
     cursor: pointer;
     background-color: #f6f6f6;
-   }
+  }
 `;
 
 const SignIn = styled(JoinNow)`
   left: 80vw;
   border: 1.2px solid #0040ff;
-  color: #0040ff;
+  color: #0a66c2;
   font-weight: 500;
-  &:hover{
-    background-color: rgba(204, 216, 255,.3);
+  &:hover {
+    background-color: rgba(204, 216, 255, 0.3);
     color: #00061a;
   }
+`;
+const Heading = styled.h1`
+  font-size: 3.5em;
+  font-weight: 100;
+  // color: rgba(102, 51, 0,.8);
+  color: rgba(153, 51, 51, 0.9);
+  border: 1px solid black;
+  max-width: 50vw;
+  position: absolute;
+  top: 16vh;
+  left: 13vw;
+  line-height: 1.2em;
+`;
+const Content = styled.div``;
+const Form = styled.form`
+  border: 1px solid red;
+  position: absolute;
+  top: 40vh;
+  height: 50vh;
+  width: 40vw;
+  padding-left: 13vw;
+  label {
+    border: 1px solid yellow;
+    font-size: 0.9em;
+    font-weight: 500;
+    color: #4c4949;
+  }
+  input {
+    display: block;
+    border-radius: 0.3em;
+    height: 7.5vh;
+    width: 100%;
+    margin-top: 0.5em;
+    border: 1px solid #4c4949;
+  }
+`;
+const ForgotPassword = styled.p`
+  color: #0a66c2;
+  margin-top: 1.5em;
+  font-weight: 500;
+`;
+const SignInButton = styled.div`
+  // border: 1px solid red;
+  margin-top: 1.2em;
+  color: #ffffff;
+  font-weight: 500;
+  background-color: #0a66c2;
+  border-radius: 2em;
+  padding: .7em;
+  text-align: center;  
+`;
+const Line = styled.div`
+font-size: .9em;
+border: 1px solid blue;
+color: black;
+text-align: center;
+background:transparent;
+padding: 1.2em;
+hr{
+  width: 50%;
+  border: 1px solid rgba(76,76,73,.3);
+  &:nth-child(1){
+    position: relative;
+    top: .8em;
+    left: -1em;
+  }
+  &:nth-child(2){
+    position: relative;
+    top: -.7em;
+    left: 14em;
+  }
+}
+`;
+const Google = styled(SignInButton)`
+background-color: #f6f6f6;
+color: black;
+color: #4c4949;
+border: 1px solid #4c4949;
+img{
+  display: inline-block;
+  // border: 1px solid red;
+  position: relative;
+  top: .3em;
+  left: -.7em;
+}
 `;
 export default Login;
