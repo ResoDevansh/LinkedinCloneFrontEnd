0f805ad7b025f4416cccb351e10f556d3b701601
ResoDevansh <sachandevansh2000@gmail.com>
2023-04-03 11:54:58 +0530

    work

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -1,11 +1,94 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Middle = () => {
-    return (<Container>Middle</Container>)
-}
+  return (
+    <Container>
+      <img id="pp" src="/images/profileimg.jpg" />
+      <button id="post">Start a post</button>
+      <Tools>
+        <button>
+          <img src="/images/phototool.svg" />
+          <span>Photo</span>
+        </button>
+        <button>
+          <img src="/images/videotool.svg" />
+          <span>Video</span>
+        </button>
+        <button>
+          <img src="/images/eventtool.svg" />
+          <span>Event</span>
+        </button>
+        <button>
+          <img src="/images/articeltool.svg" />
+          <span>Write article</span>
+        </button>
+      </Tools>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-border: 1px solid blue;`
+  border: 1px solid #dfdbda;
+  background-color: #ffffff;
+  border-radius: 0.5em;
+  height: 20%;
+  #pp{
+    height: 3em;
+    border-radius: 50%;
+    margin-left: 2%;
+    margin-top: 1.4%;
+    cursor: pointer;
+    // border: 1px solid red;
+  }
+  #post{
+    color: grey;
+    border: 1px solid grey;
+    width: 75%;
+    position: relative;
+    top: -1.3em;
+    left: 1em;
+    background-color: white;
+    border-radius: 2em;
+    padding: .8em;
+    text-align: left;
+    font-weight: 500;
+    font-size: .9em;
+    transition: all 200ms ease-in-out;
+    &:hover{
+        cursor: pointer;
+        background-color: #efedec;
+    }
+  }
+`;
 
-export default Middle
+const Tools = styled.div`
+display: flex;
+// border: 1px solid red;
+padding: 0 .2em 0 .2em;
+margin-top: -.9em;
+justify-content: space-between;
+button{
+    font-size: .9em;
+    // padding: left: .2em;
+    // padding-right: .2em;
+    cursor: pointer;
+    background-color: transparent; 
+    display: inline-flex;
+    // border: 1px solid blue;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: nowrap;
+    min-width: 20%;
+    padding: .5em 0.2em .5em 0.2em;
+    transition: all 50ms ease-in-out;
+    span{
+        // border: 1px solid red;
+        white-space: nowrap;
+    }
+    &:hover{
+        cursor: pointer;
+        background-color:#efedec;
+    }
+}`;
+export default Middle;
--- src/components/Section.js
+++ src/components/Section.js
@@ -15,17 +15,21 @@ const Section = () => {
 };
 
 const Container = styled.div`
-    // border: 1px solid red;
+    border: 1px solid red;
   position: relative;
   top: 5em;
   left: 13vw;
-  width: 72vw;
+  width: 80vw;
   display: grid;
-  grid-template-columns: minmax(14em,min-content) minmax(40%,50%) 30%;
+  grid-template-columns: minmax(14em,min-content) minmax(45%,50%) 30%;
   column-gap: 1em;
   @media only screen and (max-width: 768px) {
     width: 90vw;
     left: 2vw;
+  }
+  @media only screen and (min-width: 990px) and (max-width: 1024px){
+    width: 90vw;
+    left: 5vw;
   }
 `;
 
